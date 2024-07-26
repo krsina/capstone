721ad112edcfda71203d7c566b298bd8eef6a803
Don-Hoang <don70396@gmail.com>
2024-07-26 01:14:12 -0700

    Working on alloc form

--- client/src/components/ClubDashboard/Finance/AllocationForm.js
+++ client/src/components/ClubDashboard/Finance/AllocationForm.js
@@ -1,87 +1,67 @@
-import React from 'react'
-import FieldCounter from '../../common/FieldCounter';
-import BackButton from '../../common/BackButton';
-import OfficerField from '../../common/OfficerField';
+import React from 'react';
 
 function AllocationForm() {
-
-
     return (
-        <div className="bg-gray-300 py-20 flex flex-row items-center justify-center ml-80">
-            <BackButton />
-            <div className="w-11/12 bg-white py-20 px-24 rounded-lg">
-                <div className="bg-primary p-4 rounded-lg flex  items-center justify-center">
-                    <h1 className="text-6xl font-encode-sans font-bold text-white px-6 py-4">Club Registration</h1>
+        <div className="flex">
+            <div className="w-1/6 bg-purple-700 text-white flex flex-col p-4 space-y-4 min-h-screen">
+                <div className="text-2xl font-bold mb-8">Club Dashboard</div>
+                <a href="#" className="text-lg">Events</a>
+                <a href="#" className="text-lg">Organizations</a>
+                <a href="#" className="text-lg">Resources</a>
+                <a href="#" className="mt-auto text-lg">Logout</a>
+            </div>
+            <div className="w-5/6 bg-gray-200 py-20 flex flex-col items-center relative">
+                <div className="absolute top-0 left-0 p-6">
+                    <button className="text-primary font-bold">Back</button>
                 </div>
-
-                {/* Form */}
-                <form className="mt-8 space-y-8 text-secondary font-open-sans text-lg">
-                    <h2 className="text-3xl font-bold">Basic Information</h2>
-                    <div className="grid grid-cols-2 gap-20 mt-4">
-                        <input type="text" placeholder="Club Name" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Club Category" className="border-r border-b border-gray-300 p-2 rounded" />
+                <div className="w-4/5 bg-white py-10 px-12 rounded-lg shadow-lg">
+                    <div className="flex mb-8">
+                        <div className="w-1/2 bg-primary p-6 rounded-lg flex flex-col items-center justify-center text-white">
+                            <h1 className="text-4xl text-center font-bold">Welcome to the Allocation Form!</h1>
+                            <p className="text-lg text-center mt-4">Fill out your basic information, and break it down by line item. After approval move on to your expenditure report!</p>
+                        </div>
+                        <div className="w-1/2 pl-6">
+                            <h2 className="text-2xl font-bold bg-primary text-white rounded-lg text-center py-2">General Information</h2>
+                            <div className="space-y-4 mt-4">
+                                <input type="text" placeholder="Select your Club" className="border-gray-300 p-2 rounded w-full border-b border-r" />
+                                <div className='flex flex-row space-x-4'>
+                                    <input type="text" placeholder="Event Title" className="border-b border-r border-gray-300 p-2 rounded w-full" />
+                                    <input type="date" placeholder="Event Date" className="border-b border-r border-gray-300 p-2 rounded w-full" />
+                                </div>
+                                <textarea placeholder="Event Description (Max 150 characters)" className="border-b border-r border-gray-300 p-2 rounded w-full h-20" maxLength="150"></textarea>
+                                <div className='flex flex-row space-x-4'>
+                                    <input type="text" placeholder="Point of Contact Name" className="border-b border-r border-gray-300 p-2 rounded w-full" />
+                                    <input type="email" placeholder="Point of Contact Email" className="border-b border-r border-gray-300 p-2 rounded w-full" /> 
+                                </div> 
+                            </div>
+                        </div>
                     </div>
-
-                    <h2 className="text-3xl font-bold">Affiliation</h2>
-                    <div className="grid grid-cols-3 gap-10 mt-4">
-                        <input type="text" placeholder="Is your club affiliated?" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Name of umbrella Organizations" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Name of outside Organizations" className="border-r border-b border-gray-300 p-2 rounded" />
+                    <div className="mt-8">
+                        <h2 className="text-2xl font-bold bg-primary text-white rounded-lg text-center py-2">Allocation Breakdown</h2>
+                        <div className="flex justify-end">
+                            <button className="bg-black text-white px-4 py-2 rounded-lg my-2">ADD</button>
+                        </div>
+                        <div className="space-y-4 mt-4">
+                            <div className="flex flex-row space-x-4">
+                                <input type="text" placeholder="Item Name (ex. Decorations, Food, etc.)" className="border-b border-r border-gray-300 p-2 rounded w-full" />
+                                <input type="text" placeholder="Detail (Max 300 Characters)" className="border-b border-r border-gray-300 p-2 rounded w-full" maxLength="300" />
+                            </div>
+                            <div className="flex flex-row space-x-4">
+                                <input type="number" placeholder="Dollar Amount" className="border-b border-r border-gray-300 p-2 rounded w-full" />
+                                <input type="text" placeholder="Admin Comments" className="border-b border-r border-gray-300 p-2 rounded w-full" disabled />
+                            </div>
+                        </div>
                     </div>
-
-                    {/* Club Description */}
-                    <h2 className="text-3xl font-bold">Club Description</h2>
-                    <FieldCounter placeholder="Enter Description here" type="textarea" maxWords={300} />
-
-                    {/* Club Mission */}
-                    <h2 className="text-3xl font-bold">Mission</h2>
-                    <FieldCounter placeholder="Enter Description here" type="textarea" maxWords={300} />
-
-                    {/* Club Vision */}
-                    <h2 className="text-3xl font-bold">Vision</h2>
-                    <FieldCounter placeholder="Enter Description here" type="textarea" maxWords={300} />
-
-                    {/* Club Purpose */}
-                    <h2 className="text-3xl font-bold">Purpose</h2>
-                    <FieldCounter placeholder="Enter Description here" type="textarea" maxWords={300} />
-
-                    <h2 className="text-3xl font-bold">Meeting Information</h2>
-                    <div className="grid grid-cols-3 gap-10 mt-4">
-                        <input type="text" placeholder="Meeting Times" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Meeting Days" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Location" className="border-r border-b border-gray-300 p-2 rounded" />
+                    <div className="text-4xl text-center font-encode-sans mt-8">
+                        Total Request:
                     </div>
-
-                    <h2 className="text-3xl font-bold">Uploads</h2>
-                    <div className="grid grid-cols-2 gap-10 mt-4">
-                        <input type="file" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="file" className="border-r border-b border-gray-300 p-2 rounded" />
+                    <div className="flex justify-end">
+                        <button className="bg-black text-white px-4 py-2 rounded-lg my-2">Submit</button>
                     </div>
-
-                    <div className="flex flex-col space-y-2">
-                        <h2 className="text-3xl font-bold">Officers</h2>
-                        <p className="text-lg font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                        <OfficerField />
-                    </div>
-
-
-                    <h2 className="text-3xl font-bold">Advisor</h2>
-                    <div className="grid grid-cols-3 gap-10">
-                        <input type="text" placeholder="First Name" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Last Name" className="border-r border-b border-gray-300 p-2 rounded" />
-                        <input type="text" placeholder="Advisor Email" className="border-r border-b border-gray-300 p-2 rounded" />
-                    </div>
-                    <button
-
-                        className="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    >
-                        Submit
-                    </button>
-                </form>
-
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default AllocationForm
+export default AllocationForm;
