d7c370c6154ac5996b20aa7c2230c55b5de29295
Leonille <leonillematunan@gmail.com>
2024-08-16 02:37:13 -0700

    styling - Added styling for profile page to return user year, and club

--- client/src/components/auth/userProfile.js
+++ client/src/components/auth/userProfile.js
@@ -5,13 +5,16 @@ import BackButton from '../common/BackButton'
 import EditProfile from './Modals/editProfile';
 import { fetchUserClubs } from '../../services/ClubFunctions/clubMemberServices';
 import { NavLink } from 'react-router-dom';
+import { getPreferredClub } from '../../services/ClubFunctions/clubMemberServices';
 
 function Profile() {
     const [userDetails, setUserDetails] = useState(null);
     const { userName } = useParams(); // Gets the username from the URL
     const [isModalOpen, setIsModalOpen] = useState(false); // Used to open and close the modal
     const [userClubs, setUserClubs] = useState([]);
-    
+    const [preferredClub, setPreferredClub] = useState(null);
+    const [prefferedClubRole, setPreferredClubRole] = useState(null);
+
     const handleOpenModal = () => {
         setIsModalOpen(true); // Opens the modal that's passed in
     }
@@ -19,6 +22,14 @@ function Profile() {
     const handleCloseModal = () => {
         setIsModalOpen(false); // Closes the modal
     }
+
+    const handlePreferredClubUpdate = (newPreferredClub) => {
+        setPreferredClub(newPreferredClub); // Update the preferred club state in the Profile component
+        // Find the role associated with the preferred club
+        const clubRole = userClubs.find(club => club.club.id === newPreferredClub.id)?.role.name || 'No role';
+        setPreferredClubRole(clubRole); // Update the preferred club role state
+    }
+
 
     // Returns users id to fetch user details
     useEffect(() => {
@@ -54,19 +65,31 @@ function Profile() {
         }
     }, [userName]);
 
+    // Fetch the users preferred club
+    useEffect(() => {
+        if (preferredClub === null && userDetails) {
+            getPreferredClub(userDetails.id)
+                .then((data) => {
+                    if (data) {
+                        setPreferredClub(data.club);
+                        // Set the role of the preferred club
+                        const clubRole = userClubs.find(club => club.club.id === data.club.id)?.role.name || 'No role';
+                        setPreferredClubRole(clubRole);
+                    }
+                })
+                .catch((error) => console.error('Error fetching preferred club:', error));
+        }
+    }, [preferredClub, userDetails, userClubs]);
 
     // If the user details are not fetched yet, show a loading spinner
     if (!userDetails) {
         return <div role="status" className="flex items-center justify-center h-screen">
             <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-primary" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                {/* SVG content */}
             </svg>
             <span className="sr-only">Loading...</span>
         </div>
     }
-
-
 
     return (
         <div className="sm:ml-80 items-center justify-center pt-36 bg-gray-100 h-screen text-secondary">
@@ -80,16 +103,38 @@ function Profile() {
                         <div className="space-y-4">
                             <img className="w-36 h-36 rounded-full bg-gray-300" src={userDetails.user_image} alt="User Avatar" />
                             <h1 className="text-2xl"> {userDetails.firstname} {userDetails.lastname}</h1>
-
+                            <div className="flex flex-row space-x-6">
+                                <div className="flex flex-row items-center">
+                                    {/* Graduate Cap SVG for Year */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-primary mr-2" viewBox="0 0 64 64" fill="currentColor">
+                                        <path d="M32 2L2 18l30 16 30-16L32 2zM32 36.5l-24-12.8v10.6c0 4.8 5.6 9.1 14.5 11.2L32 58l9.5-12.5C50.4 43.4 56 39.1 56 34.3V23.7l-24 12.8z" />
+                                        <path d="M38 38.7v6.4c-2 1.1-4 2-6 2s-4-0.9-6-2v-6.4l6 3.2 6-3.2z" />
+                                    </svg>
+                                    <p className="font-open-sans text-secondary font-semibold ">{userDetails.year}</p>
+                                </div>
+                                <div className="flex flex-row  items-center">
+                                    {/* Club Icon SVG */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-primary mr-2" viewBox="0 0 64 64" fill="currentColor">
+                                        <path d="M32 2a10 10 0 00-10 10v2.7A10 10 0 1016 24v12h32V24a10 10 0 10-6-9.3V12A10 10 0 0032 2zM28 34h8v20a4 4 0 11-8 0V34z" />
+                                    </svg>
+                                    <p className="font-open-sans text-secondary font-semibold ">{preferredClub ? preferredClub.name : 'No preferred'}</p>
+                                    <p className="font-open-sans text-secondary font-semibold ml-2">{prefferedClubRole}</p>
+                                </div>
+                            </div>
                         </div>
                         <button
                             className="rounded-lg bg-secondary text-white px-2 py-1 w-36 h-12"
                             onClick={handleOpenModal}
                         >Edit Profile</button>
                     </div>
-                    <EditProfile isOpen={isModalOpen} closeModal={handleCloseModal} />
+                    <EditProfile
+                        isOpen={isModalOpen}
+                        closeModal={handleCloseModal}
+                        onPreferredClubUpdate={handlePreferredClubUpdate}
+                    />
                 </div>
-                <div className="flex flex-col bg-white sm:w-1/3  rounded-lg">
+
+                <section className="flex flex-col bg-white sm:w-1/3  rounded-lg">
                     <div className="w-full bg-secondary text-white rounded-t-lg p-3 text-center">
                         <h1 className="text-2xl font-encode-sans">Club Membership</h1>
                     </div>
@@ -116,7 +161,7 @@ function Profile() {
                             )
                         }
                     </div>
-                </div>
+                </section>
             </div>
 
         </div>
@@ -124,3 +169,4 @@ function Profile() {
 }
 
 export default Profile;
+
